f273e6b3989f389f17295f50bad2c992d2a3f79f
Priyank <34156281+priyank1574q@users.noreply.github.com>
2018-07-29 10:30:56 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,7 +6,8 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 years <- c("1999","2002","2005","2008")
 
-# The sum of emission for each year is now calculated and this data is now plotted. Lines are added between the points to see the trend for emssions from 1999 to 2008.
+# The sum of emission for each year is now calculated and this data is now plotted. 
+# Lines are added between the points to see the trend for emssions from 1999 to 2008.
 q1 <- tapply(NEI$Emission, NEI$year, sum)
 plot(years, q1, pch = 19, xlab = "Year", ylab = "Emissions", main = "Emission From All Sources", xlim = c(1998,2008))
 lines(years,q1)
