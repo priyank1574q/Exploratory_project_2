e53d18e8e3d9a4b2417080e78a973224014844e4
Priyank <34156281+priyank1574q@users.noreply.github.com>
2018-07-29 10:37:06 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -13,7 +13,7 @@ years <- c("1999","2002","2005","2008")
 # Note - The data could have been plotted as a panel plot in which each type of emission would have its own plow, it is mentioned as comment below.
 # Lines are added between the points to see the trend for each type of emssion from 1999 to 2008.
 data <- NEI %>% filter(fips == "24510") %>% group_by(type, year) %>% summarize(eSum = sum(Emissions))
-##ggplot(data, aes(year, eSum)) + facet_grid(.~type) + geom_line() + geom_point() + labs(x = "Year", y = "Emission") + ggtitle("Emission in Baltimore City, Maryland") + theme(axis.text.x = element_text(angle = 90))
+# ggplot(data, aes(year, eSum)) + facet_grid(.~type) + geom_line() + geom_point() + labs(x = "Year", y = "Emission") + ggtitle("Emission in Baltimore City, Maryland") + theme(axis.text.x = element_text(angle = 90))
 ggplot(data, aes(year, eSum)) + geom_line(aes(color = type)) + geom_point(aes(color = type)) + labs(x = "Year", y = "Emission") + ggtitle("Emission in Baltimore City, Maryland")
 
 # The plot is now saved in the current working directory.
